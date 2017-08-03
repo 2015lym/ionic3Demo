cd777cb9800fa429a55259ced38bddcd30a99cd9
2015lym <18622797149@163.com>
2017-08-03 22:34:46 +0800

    更新

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
@@ -22,6 +23,7 @@ import { BackButtonService } from "../services/backButton.service";
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Response } from '@angular/http';
+// import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // 接收数据用
+  listData: Object;
+  // 依赖注入
+  constructor(public navCtrl: NavController, private http: Http) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad() {
+    // 网络请求
+    this.http.request('http://jsonplaceholder.typicode.com/photos')
+      .subscribe((res: Response) => {
+        this.listData = res.json();
+      });
+
+    // this.http.request('http://jsonplaceholder.typicode.com/photos')
+    //   .toPromise()
+    //   .then(res => { this.listData = res.json(); })
+    //   .catch(err => { console.error(err) });
   }
 
 }
